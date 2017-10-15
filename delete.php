1c6f480f74583d2f9493f4d5785b73ec6eb55f0e
olivorel <olivorel45@hotmail.fr>
2017-10-15 22:42:47 +0200

    modification du fichier delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_GET['id'])) {
     //Vérifie si un fichier ou un dossier existe
-    if (file_exists('./upload/' . $_GET['id'])) {
+    if (file_exists('upload/' . $_GET['id'])) {
         //bool unlink ( string $filename [, resource $context ] )
         unlink("upload/" . $_GET['id']);
         header("location: index.php");
